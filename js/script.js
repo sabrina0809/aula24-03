3a4f67cff39d3fd0a886b7abadd2197a5e6f745b
sabrina0809 <souzalaurindo49@gmail.com>
2025-03-24 22:29:15 -0300

    fazendo calculadora

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function somar() {
+    let numero1 = parseFloat (document.getElementById('numero1').value);
+     let numero2 = parseFloat (document.getElementById('numero2').value);
+    
+    let resultado = numero1 + numero2;
+
+    document.getElementById('resultado').textContent = resultado;
+} 
